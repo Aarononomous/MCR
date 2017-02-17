1b6c982ff0b77232f04e95ddce23f98abe082969
Aarononomous <aaron@aaron-jacobson.com>
2017-02-16 22:14:48 -0600

    Converted shape-handling to shapely.

--- MCR.py
+++ MCR.py
@@ -1,120 +1,11 @@
 from math import *
 import shapely as sh
+from shapely.geometry import *
+from shapely.affinity import *
 import graphviz as gv
 import random as rand
 import matplotlib.pyplot as plt
 import re
-
-
-class Polygon:
-
-    '''
-    Simple (non-self-intersecting) polygons. This is a bare wrapper around the
-    actual representation as a list of (x, y) tuples in CCW order. Do note that
-    unlike some polygon formats (e.g. SVG) the start/end point is *not*
-    duplicated.
-    Please try to use Polygon objects as immutable objects: all functions return
-    new Polygons.
-    '''
-
-    def __init__(self, points):
-        self.points = points
-
-    # Inspection Methods
-
-    def bounds(self):
-        '''
-        Returns the [(x_min, y_min), (x_max, y_max)] bounding box of a set of
-        points (probably a shape)
-        '''
-        xs, ys = zip(*self.points)
-        return [(min(xs), min(ys)), (max(xs), max(ys))]
-
-    def is_convex(self):
-        # TODO
-        for pt in self.points:
-            if not (__on_left_hand__(pt, (self.points[:2]))
-                    and __on_left_hand__(self.points[0],
-                                         (self.points[-1], pt))):
-                return False
-        return True
-
-    # Transform Methods
-    # These follow the same form as SVG:
-    # https://www.w3.org/TR/SVG/coords.html
-
-    def matrix(self, a, b, c, d, e, f):
-        '''
-        Transforms the shape by multiplying matrix [ [a c e] [b d f] [0 0 1] ]
-        by [ [x_1...x_n] [y_1...y_n] 1] and returning the top two (x and y)
-        rows.
-        '''
-        return [(a*x + c*y + e, b*x + d*y + f) for (x, y) in self.points]
-
-    def translate(self, t_x, t_y=0.0):
-        '''
-        Translates a set of points (probably a shape) by vector (t_x, t_y).
-        '''
-        t_x, t_y = vector
-        return [(x + t_x, y + t_y) for (x, y) in self.points]
-
-    def scale(self, s_x, s_y=None):
-        '''
-        Rescales a set of points (probably a shape) by s_x and s_y amount.
-        If s_y is not provided, it is assumed to be the same as s_x.
-        '''
-        s_y = s_y if s_y else s_x
-        return [(s_x * x, s_y * y) for (x, y) in self.points]
-
-    def rotate(self, angle, c_x=0, c_y=0):
-        '''
-        Rotates a set of points (probably a shape) around (0,0) by angle.
-        If c_x and c_y are provided, rotate around (c_x, c_y)
-        '''
-        s = sin(angle)
-        c = cos(angle)
-
-        return [(c*(x + c_x) - s*(y + c_y) - c_x,
-                 c*(y + c_y) + s*(x + c_x) - c_y) for (x, y) in self.points]
-
-    def skew_x(self, a):
-        '''
-        Skews the polygon along the x-axis by a radians
-        '''
-        return [(x + tan(a)*y, y) for (x, y) in self.points]
-
-    def skew_y(self, skew):
-        '''
-        Skews the polygon along the y-axis by a radians
-        '''
-        return [(x, tan(a)*x + y) for (x, y) in self.points]
-
-    # Set-Theoretic Methods
-
-    # Helper methods
-
-    def __on_left_hand__(point, edge):
-        '''
-        Is point on the left-hand side of the ray edge[0] - edge[1]?
-        This includes being on the line itself
-        '''
-        px, py = point
-        x1, y1 = edge[0]
-        x2, y2 = edge[1]
-
-        # special case for vertical lines (no slope)
-        if (x1 == x2):
-            return (px < x1) == (y1 < y2)
-
-        # find the slope and y-intersect, then see if point is above or below the
-        # ray
-        m = (y2 - y1) / (x2 - x1)
-        b = y1 - m * x1
-        dist = py - (m * px + b)
-
-        # if it's above and the ray is rightward, or below and the ray is leftward,
-        # it's on the left-hand side
-        return (dist >= 0) == (x1 < x2)
 
 
 class MCR:
@@ -131,24 +22,85 @@ class MCR:
         Create an empty square to add shapes to.
         '''
         self.obstacles = []
+        self.overlapped_obstacles = []
         self.graph = []
         self.start = (0.05, 0.05)
         self.goal = (0.95, 0.95)
         # self.field = plt.Polygon([(0, 0), (0, 1), (1, 1), (1, 0)])
 
+        # All shapes are labeled. A shape can be given an explicit label, or it
+        # will be given a numerical label
+        self.__label = 0
+
         if svg:
             # initialize from svg file
-            obstacles = __shapes_from_SVG__(self, svg)
-            for o in obstacles:
-                self.add_obstacle(o)
+            obstacles = MCR.__shapes_from_SVG(svg)
+            try:
+                for o in obstacles:
+                    self.add_obstacle(o)
+            except:
+                print('Couldn\'t load all shapes')
 
-    def add_obstacle(self, shape, at=(0, 0)):
-        self.obstacles.append(shape.translate(*at))
+    def add_obstacle(self, shape):
+        '''
+        Handles adding new obstacles to the field
+        '''
+
+        if not hasattr(shape, 'label'):
+            new_label = str(self.__label)
+            self.__label += 1
+        else:
+            new_label = shape.label
+
+        shape.label = new_label
+        self.obstacles.append(shape)
 
         # TODO: clip translated shape to field bounds
-        #   - is this necessary?
-        # TODO: add shape and overlaps to graph as well
-        #   - or should this be done in two separate steps?
+        #   - is this necessary? YES
+        
+        # for every shape that is added, it may intersect with any other
+        overlaps = [o_o for o_o in self.overlapped_obstacles if o_o.intersects(shape)]
+        
+        if overlaps:
+            for o_o in overlaps:
+                self.overlapped_obstacles.remove(o_o)
+                # re-add any part which doesn't overlap
+                unoverlapped_part = o_o - shape
+                overlapped_part = o_o & shape
+                
+                if type(unoverlapped_part) == Polygon:
+                    unoverlapped_part.label = o_o.label
+                    self.overlapped_obstacles.append(unoverlapped_part)
+                elif type(unoverlapped_part) == MultiPolygon:
+                    for p in unoverlapped_part:
+                        p.label = o_o.label
+                        self.overlapped_obstacles.append(p)
+                # otherwise the new piece is wholly within unoverlapped_part
+
+                if type(overlapped_part) == Polygon:
+                    overlapped_part.label = o_o.label + ', ' + new_label
+                    self.overlapped_obstacles.append(overlapped_part)
+                elif type(overlapped_part) == MultiPolygon:
+                    for p in overlapped_part:
+                        p.label = o_o.label + ', ' + new_label
+                        self.overlapped_obstacles.append(p)
+                # otherwise the new piece is wholly within overlapped_part
+
+                # continue adding whatever parts remain
+                shape = shape - o_o
+
+            # anything that's not overlapping can be added now
+            if type(shape) == Polygon:
+                shape.label = new_label
+                self.overlapped_obstacles.append(shape)
+            elif type(shape) == MultiPolygon:
+                for p in shape:
+                    p.label = new_label
+                    self.overlapped_obstacles.append(p)
+        
+        else: # nothing overlaps!
+            shape.label = new_label
+            self.overlapped_obstacles.append(shape)
 
     def remove_obstacle(self, label):
         '''
@@ -160,7 +112,7 @@ class MCR:
         # Displaying the MCR
         pass
 
-    def show_obstacles(self):
+    def show_bare_obstacles(self):
         '''
         Outputs the square
         '''
@@ -169,11 +121,60 @@ class MCR:
 
         # add obstacles
         for obstacle in self.obstacles:
-            poly = plt.Polygon(obstacle, **self.display_opts)
+            # TODO: color
+            opts = MCR.display_opts.copy()
+            if hasattr(obstacle, 'facecolor'):
+                opts['facecolor'] = obstacle.facecolor
+            if hasattr(obstacle, 'edgecolor'):
+                opts['edgecolor'] = obstacle.edgecolor
+            poly = plt.Polygon(obstacle.exterior.coords, **opts)
             plt.gca().add_patch(poly)
+
+            if hasattr(obstacle, 'label'):
+                r_p = obstacle.representative_point()
+                plt.text(r_p.x, r_p.y, obstacle.label,
+                         horizontalalignment='center',
+                         verticalalignment='center')
+            print(obstacle.label + ": " + str(obstacle.area))
 
         plt.axis()  # set to [0,1]
         plt.show()
+
+    def show_obstacles(self, recreate=True):
+        '''
+        Outputs the square
+        '''
+        # add start and goal
+        plt.scatter(*zip(self.start, self.goal), color='red')
+
+        # add obstacles
+        for obstacle in self.overlapped_obstacles:
+            # TODO: color
+            opts = MCR.display_opts.copy()
+            if hasattr(obstacle, 'facecolor'):
+                opts['facecolor'] = obstacle.facecolor
+            if hasattr(obstacle, 'edgecolor'):
+                opts['edgecolor'] = obstacle.edgecolor
+            poly = plt.Polygon(obstacle.exterior.coords, **opts)
+            plt.gca().add_patch(poly)
+
+            if hasattr(obstacle, 'label'):
+                r_p = obstacle.representative_point()
+                plt.text(r_p.x, r_p.y, obstacle.label,
+                         horizontalalignment='center',
+                         verticalalignment='center')
+            
+            print(obstacle.label + ": " + str(obstacle.area))
+        plt.axis()  # set to [0,1]
+        plt.show()
+
+    def create_graph(self):
+        '''
+        Create the intersection graph. This should be done anytime an object is
+        added or removed, and whenever the start/goal locations change.
+        Note that this is called by default every time show_obstacles() is run.
+        '''
+        pass
 
     def show_graph(self):
         '''
@@ -192,13 +193,14 @@ class MCR:
 
     # Helper methods
 
-    def __shapes_from_SVG__(self, svg_file):
+    def __shapes_from_SVG(svg_file):
         try:
             f = open(svg_file)
-            svg = f.readlines()
+            svg = f.read()
             f.close()
         except FileNotFoundError:
             print('File {} not found'.format(svg_file))
+            return
 
         # Now start parsing
 
@@ -208,19 +210,20 @@ class MCR:
         #  Viewbox - this will be scaled to 1, 1 eventually
         try:
             viewbox = [float(x)
-                       for x in re.findall('viewBox="(.*?)"', s)[0].split()]
+                       for x in re.findall('viewBox="(.*?)"', svg)[0].split()]
             vb_w = viewbox[2] - viewbox[0]
             vb_h = viewbox[3] - viewbox[1]
         except:
             print('Can\'t find a viewbox!')
+            return
 
         # Rectangles
-        rects = re.findall('<rect.*?\/>', s)
+        rects = re.findall('<rect.*?\/>', svg)
         for r in rects:
-            x_ = re.findall('x="([\d.]+)"', r)
+            x_ = re.findall('x="([-\d.]+)"', r)
             x = float(x_[0]) if x_ else 0.0
 
-            y_ = re.findall('y="([\d.]+)"', r)
+            y_ = re.findall('y="([-\d.]+)"', r)
             y = float(y_[0]) if y_ else 0.0
 
             h_ = re.findall('height="([\d.]+)"', r)
@@ -229,46 +232,43 @@ class MCR:
             w_ = re.findall('width="([\d.]+)"', r)
             w = float(w_[0])
 
-            rect = sh.Polygon([(x, y), (x + w, y), (x + w, y + h), (x, y + h)])
+            rect = Polygon([(x, y), (x + w, y), (x + w, y + h), (x, y + h)])
 
             # Transforms
-            transform = re.search('transform="(.*?)"', r)
+            transform = re.findall('transform="(.*?)"', r)
             if transform:
-                t_list = re.split(',\w*', transform)
-                for t in t_list:
+                t_list = re.findall('\w+\(.+?\)', transform[0])
+                for t in reversed(t_list):  # reverse to accumulate (compose)
                     if t.startswith('matrix'):
-                        vals = re.findall('matrix\w*\((.*?)\)', t)[0]
-                        rect = sh.affinity.affine_transform(
-                            rect, *[float(x) for x in re.split(',\w*', vals)]
-                        )
+                        vals = re.findall('\((.+)\)', t)[0]
+                        a,d,b,e,xoff,yoff = [float(x) for x in re.split(',?\s+', vals)]
+                        rect = affine_transform(
+                            rect, [a, b, d, e, xoff, yoff])
                     elif t.startswith('translate'):
-                        vals = re.findall('translate\w*\((.*?)\)', t)[0]
-                        rect = sh.affinity.translate(
-                            rect, *[float(x) for x in re.split(',\w*', vals)]
-                        )
+                        vals = re.findall('\((.+)\)', t)[0]
+                        x, y = (float(x) for x in re.split('\s+,?\s*', vals))
+
+                        rect = translate(rect, x, y)
                     elif t.startswith('scale'):
-                        vals = re.findall('scale\w*\((.*?)\)', t)[0]
-                        rect = sh.affinity.scale(
-                            rect,
-                            *[float(x) for x in re.split(',\w*', vals)],
-                            origin=(0, 0)
-                        )
+                        vals = re.findall('\((.+)\)', t)[0]
+                        rect = scale(rect,
+                                     *[float(x) for x in re.split('\s+,?\s*',
+                                                                  vals)],
+                                     origin=(0, 0))
                     elif t.startswith('rotate'):
-                        val = re.findall('scale\w*\((.*?)\)', t)[0]
-                        rect = sh.affinity.rotate(
-                            rect, float(val), origin=(0, 0), use_radians=True
-                        )
+                        val = re.findall('\((.+)\)', t)[0]
+                        rect = rotate(rect, float(val), origin=(0, 0))
                     elif t.startswith('skewX'):
-                        val = re.findall('scale\w*\((.*?)\)', t)[0]
-                        rect = rect.skew_x(float(vals))
+                        val = re.findall('\((.+)\)', t)[0]
+                        rect = skew(rect, xs=float(vals), origin=(0, 0))
                     elif t.startswith('skewY'):
-                        val = re.findall('scale\w*\((.*?)\)', t)[0]
-                        rect = rect.skew_y(float(vals))
+                        val = re.findall('\((.+?)\)', t)[0]
+                        rect = skew(rect, ys=float(vals), origin=(0, 0))
 
             shapes.append(rect)
 
         # Polygons
-        polygons = re.findall('<polygon.*?\/>', s)
+        polygons = re.findall('<polygon.*?\/>', svg)
         for p in polygons:
             polygon = []
             point_list = re.findall('points="(.*?)"', p)
@@ -279,13 +279,18 @@ class MCR:
                 polygon.append((points.pop(), points.pop()))
             polygon.pop()  # remove the doubled last point
 
-            shapes.append(sh.Polygon(polygon))
+            shapes.append(Polygon(polygon))
 
-        # N.b.: the svg viewbox starts at the top left
         # rescale to [1,1]
+        # N.b.: the svg viewbox starts at the top left
         for shape in shapes:
-            scaled_shapes.append(
-                [(x / vb_w, 1 - y / vb_h) for (x, y) in shape])
+            shape = translate(scale(
+                shape,
+                xfact=1 / vb_w,
+                yfact=-1 / vb_h,
+                origin=(0, 0)
+            ), 0, 1)
+            scaled_shapes.append(shape)
 
         return scaled_shapes
 
@@ -299,41 +304,41 @@ def solve_mcr(mcr):
     pass
 
 
-def random_mcr(obstacles=10):
+def random_mcr(obstacles=10, scale_factor=0.25):
     '''
     Creates an MCR with random polygonal obstacles
     '''
     mcr = MCR()
-    for _ in range(obstacles):
-        x = rand.random()
-        y = rand.random()
-        mcr.add_obstacle(random_shape(), at=(x, y))
-        return mcr
+    pts = poisson_2d(obstacles)
+    new_obs = []
+
+    for i in range(obstacles):
+        r = scale(random_shape(), scale_factor, scale_factor)
+        x, y = pts.pop()
+        minx, miny, _, _ = r.bounds
+        new_obs.append(translate(r, x - minx, y - miny))
+
+    # rescale again, to fit all shapes in the
+    bounds = [o.bounds for o in new_obs]
+    max_x = max([b[2] for b in bounds])
+    max_y = max([b[3] for b in bounds])
+
+    for obstacle in new_obs:
+        mcr.add_obstacle(scale(obstacle, 1 / max_x, 1 / max_y, origin=(0, 0)))
+
+    return mcr
 
 
-def random_shape(max_sides=6, scale_factor=0.5):
+def random_shape(max_sides=6):
     '''
-    Creates a random convex polygon with vertices between [0,1] x [0,1]
-    scaled to approx. [0, scale] in either direction
+    Creates a random convex polygon with vertices between [0,1] x [0,1].
     '''
-
     shape = approx_ngon(rand.randrange(3, max_sides + 1))
 
     # rotate it randomly
-    shape = rotate(shape, by=rand.uniform(0, 2*pi))
-
-    # rescale to fit into about scale part of field
-    b = bounds(shape)
-    dims = (b[1][0] - b[0][0], b[1][1] - b[0][1])
-    max_dim = max(dims)
-    shape = scale(shape, scale_factor / max_dim)
-
-    # and set the lower left of it's bounding box to [0,0]
-    b = bounds(shape)
-    x, y = [min(dim) for dim in zip(*b)]  # minimum of the b.b.
-    shape = translate(shape, (-x, -y))
-
-    return shape
+    return rotate(shape, rand.uniform(0, 2*pi),
+                  use_radians=True,
+                  origin='centroid')
 
 
 def on_left_hand(point, edge):
@@ -381,4 +386,27 @@ def approx_ngon(n, variance=0.25):  # TODO: magic number
                 and on_left_hand(pts[0], (pts[-1], pt)):
             pts.append(pt)
         else:
-            return pts
+            return Polygon(pts)
+
+
+def poisson_2d(k):
+    '''
+    Returns an array of k (x, y) tuples evenly spaced across [0, 1] x [0, 1]
+    Note that there are tweaks to the distribution that make it unreliable
+    as an _actual_ Poisson distribution.
+    '''
+    xs = [0]
+    ys = [0]
+
+    for _ in range(k-1):
+        xs.append((xs[-1] + rand.expovariate(k+1)))
+        ys.append((ys[-1] + rand.expovariate(k+1)))
+
+    max_x = max(xs)
+    max_y = max(ys)
+
+    xs = [x / max_x for x in xs]
+    ys = [y / max_y for y in ys]
+
+    rand.shuffle(ys)
+    return list(zip(xs, ys))
